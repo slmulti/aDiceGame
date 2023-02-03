32e41e6fc918fa10803c38a3b42950e0f8768ddb
slmutli <108276889+slmulti@users.noreply.github.com>
2023-02-03 12:26:52 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,96 @@
+"use strict";
+
+//selecting elements both methods valid
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+const diceEl = document.querySelector(".dice"); //dont forget the . for classes .......and # for id I suppose
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+let scores, currentScore, activePlayer, playing;
+//starting conditions
+const init = function () {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    player0El.classList.remove("player--winner");
+    player1El.classList.remove("player--winner");
+    player0El.classList.add("player--active");
+    player1El.classList.remove("player--active");
+    diceEl.classList.add("hidden");
+};
+
+init();
+
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
+};
+
+//rolling the dice
+btnRoll.addEventListener("click", function () {
+    if (playing) {
+        //1.generate a randome dice roll
+        const dice = Math.trunc(Math.random() * 6) + 1;
+
+        //2.display dice
+        diceEl.classList.remove("hidden");
+        diceEl.src = `dice-${dice}.png`;
+
+        //check for rolled 1: if true switch to other player
+        if (dice !== 1) {
+            //add dice to current score
+            currentScore += dice;
+            document.getElementById(`current--${activePlayer}`).textContent =
+                currentScore;
+        } else {
+            //switch to next player
+            switchPlayer();
+        }
+    }
+});
+
+//hold current score
+btnHold.addEventListener("click", function () {
+    if (playing) {
+        //1.add current score to score of actrive player
+        scores[activePlayer] += currentScore;
+
+        //score[1] =  score[1] + current core
+        document.getElementById(`score--${activePlayer}`).textContent =
+            scores[activePlayer];
+        //2.check if score is >=100
+        if (scores[activePlayer] >= 100) {
+            //finsih game
+            playing = false;
+            diceEl.classList.add("hidden");
+            document
+                .querySelector(`.player--${activePlayer}`)
+                .classList.add("player--winner");
+            document
+                .querySelector(`.player--${activePlayer}`)
+                .classList.remove("player--active");
+        } else {
+            //switch to next player
+            switchPlayer();
+        }
+    }
+});
+
+//reset game
+btnNew.addEventListener("click", init);
